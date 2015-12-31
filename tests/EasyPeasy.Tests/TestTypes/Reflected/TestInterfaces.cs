5c97d40fc0b8d9d940f8cd83140fc37617617e52
Matt Channer <mchanner@gmail.com>
2015-12-31 16:54:48 +0000

    Upped NUnit.Runner version to fix Travis CI build and added a new class for reflection testing

---
+++ tests/EasyPeasy.Tests/TestTypes/Reflected/TestInterfaces.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+using EasyPeasy.Attributes;
+
+namespace EasyPeasy.Tests.TestTypes.Reflected
+{
+    /// <summary>
+    /// Annotated interface to test reflection helpers determine the correct meta data
+    /// </summary>
+    [Consumes(MediaType.ApplicationJson),
+     Produces(MediaType.ApplicationXml),
+     Path("/services/{apiVersion}")]
+    public interface ITestService1
+    {
+        [Path("/data")]
+        Task<CollectionDto> GetData([QueryParam("q")] string query);
+
+        /// <summary>
+        /// Overrides Consumes and Produces
+        /// </summary>
+        /// <param name="body">The body of the request</param>
+        /// <returns></returns>
+        [POST, Path("/update"), Consumes(MediaType.TextXml), Produces(MediaType.ApplicationOctetStream)]
+        Task Update(SimpleDto body);
+    }
+}
